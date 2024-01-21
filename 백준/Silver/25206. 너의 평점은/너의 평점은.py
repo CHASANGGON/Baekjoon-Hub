1f6eb763d5d10dc588af9cbef902ac9450564ca2
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-01-21 23:52:03 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,30 +1,14 @@
-total_grade = 0
+grade_chart = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+score_chart = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+total_score = 0
 credit = 0
 for _ in range(20):
-    a, b, c = input().split()
-    if c != 'P':
-        credit += float(b)
-        if c == 'A+':
-            grade = 4.5
-        elif c == 'A0':
-            grade = 4.0
-        elif c == 'B+':
-            grade = 3.5
-        elif c == 'B0':
-            grade = 3.0
-        elif c == 'C+':
-            grade = 2.5
-        elif c == 'C0':
-            grade = 2.0
-        elif c == 'D+':
-            grade = 1.5
-        elif c == 'D0':
-            grade = 1.0
-        else:
-            grade = 0
-        total_grade += float(b)*grade
-        
+    a = input().split()
+    if a[2] != 'P':
+        total_score += score_chart[grade_chart.index(a[2])] * float(a[1])
+        credit += float(a[1])
 if credit != 0:
-    print(f'{total_grade/credit:.6f}')
+    print(f'{total_score/credit:.6f}')
 else:
     print(f'{0:.6f}')
