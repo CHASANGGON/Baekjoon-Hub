ec791de0be0c74787e5e18523915ecd93f2d26ea
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-01-22 22:59:03 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4388 ms, Memory: 51900 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,8 @@
+n = int(input())
+dots = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    dots.append((x,y))
+dots.sort(key=lambda x : (x[0],x[1]))
+for dot in dots:
+    print(dot[0], dot[1])
