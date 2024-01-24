2a228c2d6fffde693521b210845199ee9bcecfa1
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-01-25 00:24:52 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 292 ms, Memory: 71112 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,10 +1,6 @@
 n = int(input())
 
-a = 0
-b = 1
-if n != 1:
-    for _ in range(n-1):
-        temp = b
-        b = (b + a)%1000000007  
-        a = temp
-print(b)
+dp = [0,1]
+for i in range(1,n):
+    dp.append((dp[i-1]+dp[i])%1000000007)
+print(dp[n])
