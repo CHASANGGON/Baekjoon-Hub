8dd24a0f69db07b82cdf044b5bfe048801394960
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-01-28 20:38:05 +0900

    [Silver IV] Title: 수 찾기, Time: 4936 ms, Memory: 144720 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,11 +1,10 @@
 import sys
 input()
 lst_n = list(set(map(int,sys.stdin.readline().split())))
-m = int(input())
-n = len(lst_n)
 lst_n.sort()
-
+input()
 lst_m = list(map(int,sys.stdin.readline().split()))
+
 
 for m in lst_m:
     if m in lst_n:
