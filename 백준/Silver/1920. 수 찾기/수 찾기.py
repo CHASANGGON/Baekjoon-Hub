132581d03fcdb50d0f34eb367c8a468bd1180ad5
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-01-28 21:38:09 +0900

    [Silver IV] Title: 수 찾기, Time: 204 ms, Memory: 143076 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,11 +1,8 @@
+import sys
 input()
-lst_n = set(map(int,input().split()))
-# set에서 다시 list로 만드니 탐색시간이 엄청 길어짐
-# set를 쓰면 순서가 없어서 인덱스 접근은 못 하지만
-# 멤버십 연산자는 상관없이 쓸 수 있다!!
-# 문제의 의도는 이분 탐색이라고 한다..
+lst_n = set(map(int,sys.stdin.readline().split()))
 input()
-lst_m = list(map(int,input().split()))
+lst_m = list(map(int,sys.stdin.readline().split()))
 
 for m in lst_m:
     if m in lst_n:
