8bb1820bac5bdf245de49b71f5eb484b07fc6acf
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-02-11 23:24:33 +0900

    [Silver II] Title: 좌표 압축, Time: 1112 ms, Memory: 282104 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -3,14 +3,10 @@ N = int(sys.stdin.readline())
 nums = list(map(int,sys.stdin.readline().split()))
 nums_set = list(set(nums))
 nums_set.sort()
-length = len(nums_set)
-nums_accu_sum = [0] + [1]*(length-1)
-
-for i in range(length-1):
-    nums_accu_sum[i+1] += nums_accu_sum[i]
+nums_accu_sum = range(len(nums_set))
 
 dic = {}
-for i in range(length):
+for i in range(len(nums_set)):
     dic[nums_set[i]] = nums_accu_sum[i]
 
 for num in nums:
