91ff3f51e59caba2501f18fad39b70be40841bb8
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-02-11 18:26:49 +0900

    [Gold II] Title: 버블 소트, Time: 1364 ms, Memory: 139784 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.py
+++ 백준/Gold/1377. 버블 소트/버블 소트.py
@@ -7,10 +7,11 @@ n = int(input())
 nums = [0]*n
 for i in range(n):
     nums[i] = (int(input()),i) # 입력값과 인덱스를 함께 저장
+                               # nums = [(5,0), (10,1), (1,2)~]
 
-sorted_nums = sorted(nums)
+sorted_nums = sorted(nums) # 리스트 내부 튜플의 첫 번째 요소를 기준으로 정렬
 
-idx_difference = [0]*n
+idx_difference = [0]*n # 인덱스 차이값을 담아줄 리스트
 for i in range(n):
     idx_difference[i] = sorted_nums[i][1] - nums[i][1]
 
