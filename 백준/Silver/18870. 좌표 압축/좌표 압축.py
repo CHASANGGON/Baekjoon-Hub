bf218c3e0326a550b32856dc1f8fe56b104f7521
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-02-11 23:25:55 +0900

    [Silver II] Title: 좌표 압축, Time: 1104 ms, Memory: 282104 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,13 +1,15 @@
 import sys
 N = int(sys.stdin.readline())
 nums = list(map(int,sys.stdin.readline().split()))
-nums_set = list(set(nums))
-nums_set.sort()
-nums_accu_sum = range(len(nums_set))
+
+nums_set = list(set(nums)) # 본인보다 작은 수의 종류만 따지니까 set
+nums_set.sort() # 오름차순 정렬
+
+nums_ord = range(len(nums_set)) # 압축된 좌표를 나타내는 리스트 
 
 dic = {}
 for i in range(len(nums_set)):
-    dic[nums_set[i]] = nums_accu_sum[i]
+    dic[nums_set[i]] = nums_ord[i]
 
 for num in nums:
     print(dic[num],end=' ')
