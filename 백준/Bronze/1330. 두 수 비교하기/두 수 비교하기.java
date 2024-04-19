782d9602e2b3218993358e53677eda202fa0733c
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-04-19 23:41:34 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,17 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력을 받기 위한 객체 생성
         
-        String str = br.readLine(); // 한 줄 입력 받기
-        StringTokenizer st = new StringTokenizer(str," "); // 공백을 기준으로 나누기 위한 객체 생성
+        String[] str = br.readLine().split(" ");
         
-        int A = Integer.parseInt(st.nextToken()); // 토큰을 하나씩 넘겨주면서 정수로 변환
-        int B = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(str[0]); // 토큰을 하나씩 넘겨주면서 정수로 변환
+        int B = Integer.parseInt(str[1]);
 
         if(A > B) System.out.println(">");
         else if(A < B) System.out.println("<");
