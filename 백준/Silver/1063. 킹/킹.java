d4fd0d0d26cce0ce4414a5d62da6c2ac66bc37bd
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2024-11-25 17:40:54 +0900

    [Silver III] Title: 킹, Time: 116 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.java
+++ 백준/Silver/1063. 킹/킹.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.sql.SQLOutput;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,188 +8,55 @@ public class Main {
 
         String king = st.nextToken();
         String stone = st.nextToken();
-
-        int kingC = king.charAt(0) - 64;
-        int kingR = Integer.parseInt(String.valueOf(king.charAt(1)));
-
-        int stoneC = stone.charAt(0) - 64;
-        int stoneR = Integer.parseInt(String.valueOf(stone.charAt(1)));
-
         int n = Integer.parseInt(st.nextToken());
 
+        int kingC = king.charAt(0) - 64;
+        int kingR = king.charAt(1) - '0';
+        int stoneC = stone.charAt(0) - 64;
+        int stoneR = stone.charAt(1) - '0';
+
+        // 방향 이동 정의
+        Map<String, int[]> directions = new HashMap<>();
+        directions.put("R", new int[]{1, 0});
+        directions.put("L", new int[]{-1, 0});
+        directions.put("B", new int[]{0, -1});
+        directions.put("T", new int[]{0, 1});
+        directions.put("RT", new int[]{1, 1});
+        directions.put("LT", new int[]{-1, 1});
+        directions.put("RB", new int[]{1, -1});
+        directions.put("LB", new int[]{-1, -1});
+
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
+            String move = br.readLine();
+            int[] dir = directions.get(move);
 
-            switch (s) {
-                case "R":
+            // 킹 이동
+            int nextKingC = kingC + dir[0];
+            int nextKingR = kingR + dir[1];
 
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingC + 1 == stoneC && kingR == stoneR) {
-                        // 돌이 체스판을 벗어나지 않는다면
-                        if (stoneC + 1 <= 8) {
-                            // 돌과 체스판을 같이 이동
-                            stoneC += 1;
-                            kingC += 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingC + 1 <= 8) {
-                            // 킹만 이동
-                            kingC += 1;
-                        }
-                    }
+            // 돌과의 충돌 확인
+            if (nextKingC == stoneC && nextKingR == stoneR) {
+                int nextStoneC = stoneC + dir[0];
+                int nextStoneR = stoneR + dir[1];
 
-                    break;
-
-                case "L":
-
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingC - 1 == stoneC && kingR == stoneR) {
-                        // 돌이 체스판을 벗어나지 않는다면
-                        if (stoneC - 1 >= 1) {
-                            // 돌과 체스판을 같이 이동
-                            stoneC -= 1;
-                            kingC -= 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingC - 1 >= 1) {
-                            // 킹만 이동
-                            kingC -= 1;
-                        }
-                    }
-                    break;
-
-                case "B":
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingR - 1 == stoneR && kingC == stoneC) {
-                        if (stoneR - 1 >= 1) {
-                            // 돌과 체스판을 같이 이동
-                            stoneR -= 1;
-                            kingR -= 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingR - 1 >= 1) {
-                            // 킹만 이동
-                            kingR -= 1;
-                        }
-                    }
-                    break;
-
-                case "T":
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingR + 1 == stoneR && kingC == stoneC) {
-                        if (stoneR + 1 <= 8) {
-                            // 돌과 체스판을 같이 이동
-                            stoneR += 1;
-                            kingR += 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingR + 1 <= 8) {
-                            // 킹만 이동
-                            kingR += 1;
-                        }
-                    }
-                    break;
-
-                case "RT":
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingR + 1 == stoneR && kingC + 1 == stoneC) {
-                        if (stoneR + 1 <= 8 && stoneC + 1 <= 8) {
-                            // 돌과 체스판을 같이 이동
-                            stoneR += 1;
-                            stoneC += 1;
-                            kingR += 1;
-                            kingC += 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingR + 1 <= 8 && kingC + 1 <= 8) {
-                            // 킹만 이동
-                            kingR += 1;
-                            kingC += 1;
-                        }
-                    }
-                    break;
-
-                case "LT":
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingR + 1 == stoneR && kingC - 1 == stoneC) {
-                        if (stoneR + 1 <= 8 && stoneC - 1 >= 1) {
-                            // 돌과 체스판을 같이 이동
-                            stoneR += 1;
-                            stoneC -= 1;
-                            kingR += 1;
-                            kingC -= 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingR + 1 <= 8 && kingC - 1 >= 1) {
-                            // 킹만 이동
-                            kingR += 1;
-                            kingC -= 1;
-                        }
-                    }
-                    break;
-
-                case "RB":
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingR - 1 == stoneR && kingC + 1 == stoneC) {
-                        if (stoneR - 1 >= 1 && stoneC + 1 <= 8) {
-                            // 돌과 체스판을 같이 이동
-                            stoneR -= 1;
-                            stoneC += 1;
-                            kingR -= 1;
-                            kingC += 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingR - 1 >= 1 && kingC + 1 <= 8) {
-                            // 킹만 이동
-                            kingR -= 1;
-                            kingC += 1;
-                        }
-                    }
-                    break;
-
-                case "LB":
-                    // 킹을 돌이 있는 위치로 움직여야 한다면
-                    if (kingR - 1 == stoneR && kingC - 1 == stoneC) {
-                        // 돌이 체스판을 벗어나지 않는다면
-                        if (stoneR - 1 >= 1 && stoneC - 1 >= 1) {
-                            // 돌과 체스판을 같이 이동
-                            stoneR -= 1;
-                            stoneC -= 1;
-                            kingR -= 1;
-                            kingC -= 1;
-                        }
-                        // 킹 혼자 움직인다면
-                    } else {
-                        // 킹이 체스판을 벗어나지 않는다면
-                        if (kingR - 1 >= 1 && kingC - 1 >= 1) {
-                            // 킹만 이동
-                            kingR -= 1;
-                            kingC -= 1;
-                        }
-                    }
-                    break;
-
-                default:
-                    break;
+                // 돌이 체스판 안에 있어야 이동 가능
+                if (nextStoneC >= 1 && nextStoneC <= 8 && nextStoneR >= 1 && nextStoneR <= 8) {
+                    stoneC = nextStoneC;
+                    stoneR = nextStoneR;
+                    kingC = nextKingC;
+                    kingR = nextKingR;
+                }
+            } else {
+                // 킹이 체스판 안에 있어야 이동 가능
+                if (nextKingC >= 1 && nextKingC <= 8 && nextKingR >= 1 && nextKingR <= 8) {
+                    kingC = nextKingC;
+                    kingR = nextKingR;
+                }
             }
         }
 
-        System.out.println((char) (kingC + 64) + String.valueOf(kingR));
-        System.out.println((char) (stoneC + 64) + String.valueOf(stoneR));
-
+        // 결과 출력
+        System.out.println(String.format("%c%d", kingC + 64, kingR));
+        System.out.println(String.format("%c%d", stoneC + 64, stoneR));
     }
 }
