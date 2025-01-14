0c06a8f0a4a26ff7b04171e16060d88b6398d65d
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-01-14 15:00:37 +0900

    [Silver I] Title: 최대공약수, Time: 240 ms, Memory: 20880 KB -BaekjoonHub

--- 백준/Silver/2824. 최대공약수/최대공약수.java
+++ 백준/Silver/2824. 최대공약수/최대공약수.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
         // N 입력 받기
         int N = Integer.parseInt(br.readLine());
         String[] numsN = br.readLine().split(" ");
@@ -28,19 +29,24 @@ public class Main {
         for (int divisorA : mapA.keySet()) {
             if (mapB.containsKey(divisorA)) {
                 int minCount = Math.min(mapA.get(divisorA), mapB.get(divisorA));
+                // gcd *= (int) Math.pow(divisorA, minCount);
+                // 반복문을 사용하는 것이 더 안전하다 -GPT-
                 for (int i = 0; i < minCount; i++) {
                     gcd *= divisorA;
-                    if (gcd >= 1_000_000_000) { 
+                    // 문제에서 9자리보다 길다면, 마지막 9자리만 출력하라고 한다
+                    // 5,124,312,312 -> 124,312,312
+                    // 10번 째 자리 부터는 몇을 곱하든 표시할 필요가 없기 때문에 기억할 필요가 없다
+                    // 오버플로우 때문에 그런 것이다. 어쨋든 기억할 필요가 없으니 지운다.
+                    if (gcd >= 1000000000) {
                         flag = true;
-                        gcd %= 1_000_000_000;
+                        gcd %= 1000000000;
                     }
                 }
             }
         }
 
-        // ✅ flag가 true면 항상 9자리로 고정 출력
         if (flag) {
-            System.out.printf("%09d\n", gcd);
+            System.out.printf("%09d", gcd); // 나머지 자리를 0으로 채워서 출력
         } else {
             System.out.println(gcd);
         }
@@ -54,7 +60,7 @@ public class Main {
             }
         }
 
-        if (num > 1) { // ✅ num이 소수인 경우 처리
+        if (num != 1) { // num이 소수인 경우 처리
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
     }
