7f1f1ede383e92988c11f6703ae46ee01c88ad62
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-01-22 17:32:20 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 252 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -11,7 +11,7 @@ public class Main {
         // N, K 입력 받기
         String[] NK = br.readLine().split(" ");
         N = Integer.parseInt(NK[0]); // 길이가 2N인 벨트
-        K = Integer.parseInt(NK[1]); // 내구도가 0인 칸의 개수가 K개 이상이라면 종료
+        K = Integer.parseInt(NK[1]); // 내구도가 0인 칸의 개수가 K개 이상이라면 과정을 종료한다
 
         // 벨트의 내구도 입력 받기
         durability = new int[2 * N];
@@ -23,13 +23,13 @@ public class Main {
         // 로봇 위치 배열
         robots = new boolean[2 * N];
 
-        System.out.println(conveyorOperation());
+        conveyorOperation();
     }
 
-    private static int conveyorOperation() {
-        int stage = 0;
+    private static void conveyorOperation() {
+        int stage = 0; // 단계를 저장할 변수
 
-        while (true) {
+        while (countZeroDurability()) {
             stage++;
 
             // 1. 컨베이어 벨트 회전
@@ -40,25 +40,36 @@ public class Main {
 
             // 3. 올리는 위치에 로봇 올리기
             placeRobot();
-
-            // 4. 내구도 0인 칸 개수 체크
-            if (countZeroDurability() >= K) break;
         }
 
-        return stage;
+        // 출력
+        System.out.println(stage);
     }
 
+    // 내구도가 0인 칸의 개수가 K개 이상이라면 과정을 종료한다
+    private static boolean countZeroDurability() {
+        int cnt = 0;
+        for (int dur : durability) {
+            if (dur == 0) {
+                cnt++;
+            }
+        }
+        return cnt < K;
+    }
+
+    // 컨베이어 벨트 회전
     private static void rotateBelt() {
-        // 벨트 회전
         int lastDurability = durability[2 * N - 1];
+        boolean lastRobot = robots[2 * N - 1];
+
         for (int i = 2 * N - 1; i > 0; i--) {
             durability[i] = durability[i - 1];
             robots[i] = robots[i - 1];
         }
         durability[0] = lastDurability;
-        robots[0] = false;
+        robots[0] = lastRobot;
 
-        // 내리는 위치에서 로봇 내리기
+        // 내리는 위치
         robots[N - 1] = false;
     }
 
@@ -69,24 +80,18 @@ public class Main {
                 robots[i - 1] = false;
                 durability[i]--;
             }
+
         }
 
-        // 내리는 위치에서 로봇 내리기
-        robots[N - 1] = false;
+        robots[N - 1] = false; // 내리는 위치에서 로봇 내리기
     }
 
+    // 올리는 위치에서 로봇 올리기
     private static void placeRobot() {
         if (durability[0] > 0) {
             robots[0] = true;
             durability[0]--;
         }
     }
-
-    private static int countZeroDurability() {
-        int count = 0;
-        for (int dur : durability) {
-            if (dur == 0) count++;
-        }
-        return count;
-    }
 }
+
