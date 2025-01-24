8aea3b5a3fc44209e433c54f8fb730a7ed8ae34b
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-01-24 15:19:43 +0900

    [Gold V] Title: 경쟁적 전염, Time: 176 ms, Memory: 18652 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -3,12 +3,10 @@ import java.util.*;
 
 public class Main {
     private static StringTokenizer st;
-    private static int size, virus, N, K, S, X, Y, i, j, ni, nj;
-    private static final int[] di = {1, -1, 0, 0};
-    private static final int[] dj = {0, 0, 1, -1};
-    private static int[] coordinates;
+    private static int size, N, K, S, X, Y, i, j, ni, nj;
     private static int[][] testTube;
-    private static Queue<int[]> virusCoordinates, tempVirueCoordinates; // 메모리 효율성을 위해 클래스 생성없이 배열 자료형 사용
+    private static final int[][] dij = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private static Queue<int[]> coordinates;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,24 +17,11 @@ public class Main {
         K = Integer.parseInt(NK[1]); // 바이러스의 번호는 K이하의 자연수
 
         // 시험관 입력 받기
-        // 좌표를 저장할 Queue(처음에는 바이러스의 우선순위대로 실행해야하므로 우선순위 Queue를 사용)
-        virusCoordinates = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                // a와 b는 좌표 (i, j) 형태
-                // testTube[a[0]][a[1]]와 testTube[b[0]][b[1]]를 비교하여 우선순위 결정
-                return Integer.compare(testTube[a[0]][a[1]], testTube[b[0]][b[1]]);
-            }
-        });
         testTube = new int[N][N];
         for (i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (j = 0; j < N; j++) {
-                virus = Integer.parseInt(st.nextToken());
-                testTube[i][j] = virus;
-                if (virus > 0) {
-                    virusCoordinates.offer(new int[]{i, j});
-                }
+                testTube[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
@@ -46,43 +31,49 @@ public class Main {
         X = Integer.parseInt(SXY[1]);
         Y = Integer.parseInt(SXY[2]);
 
-        // S초 실행
-        tempVirueCoordinates = new LinkedList<>(); // 1초 동안의 실행을 위해 임시 저장용 Queue
-        while (S-- > 0) {
-            size = virusCoordinates.size();
-            while (size-- > 0) {
-                coordinates = virusCoordinates.poll();
-                i = coordinates[0];
-                j = coordinates[1];
-                for (int k = 0; k < 4; k++) {
-                    ni = i + di[k];
-                    nj = j + dj[k];
-                    if (indexCheck(ni, nj) && testTube[ni][nj] == 0) {
-                        testTube[ni][nj] = testTube[i][j]; // 전염
-                        tempVirueCoordinates.offer(new int[]{ni, nj}); // 추가
+        // 목표 좌표에 이미 바이러스가 있으면 그 번호를 바로 출력하고 종료
+        if (testTube[X - 1][Y - 1] != 0) { // 바이러스 번호가 이미 존재하면
+            System.out.print(testTube[X - 1][Y - 1]);
+            return; // 조기 종료
+        }
+
+        // 바이러스가 없는 경우
+        ArrayList<Integer> result = new ArrayList<>();
+        coordinates = new LinkedList<>();
+        coordinates.offer(new int[]{X - 1, Y - 1}); // 관심 대상 좌표에서 역으로 출발
+        testTube[X - 1][Y - 1] = -1; // 해당 좌표를 기록(전염 시작점)
+
+        while (S-- > 0) { // S초 실행
+            size = coordinates.size();
+            while (size-- > 0) { // while문이 모두 실행될 때마다 1초
+                int[] now = coordinates.poll();
+                for (int[] d : dij) {
+                    ni = now[0] + d[0];
+                    nj = now[1] + d[1];
+                    if (indexCheck(ni, nj)) {
+                        if (testTube[ni][nj] == 0) { // 아직 전염되지 않았다면
+                            testTube[ni][nj] = -1; // 지나온 것을 기록
+                            coordinates.offer(new int[]{ni, nj}); // 추가
+                        } else if (testTube[ni][nj] > 0) { // 바이러스가 존재한다면 -> 해당 위치에서 출발한 바이러스가 목표 지점을 전염 시킬 수 있다는 것
+                            result.add(testTube[ni][nj]); // 바이러스 번호를 추가
+                        }
                     }
                 }
             }
 
-            if (testTube[X - 1][Y - 1] > 0) {
-                System.out.println(testTube[X - 1][Y - 1]);
-                return;
-            }
-
-            // 첫 실행 이후부터는 우선순위 Queue 사용하지 않아도 된다.
-            // 처음에 우선순위를 사용해서 바이러스를 전염시켰으므로
-            // 이후부터는 순서대로 꺼내기만 해도 우선순위대로 꺼내진다.
-            // 우선순위 큐는 O(logN) > 링크드리스트는 O(1)
-            // 따라서 시간복잡도가 높은 우선순위 큐를 굳이 계속 사용할 이유가 없다
-            virusCoordinates = new LinkedList<>();
-            size = tempVirueCoordinates.size();
-            while (size-- > 0) {
-                virusCoordinates.offer(tempVirueCoordinates.poll());
+            // 1초가 지날 때 마다 조기 종료 체크
+            if (result.size() > 0) {
+                int virus = 1001; // 바이러스는 1 ~ 1000번
+                for (int a : result) {
+                    virus = Math.min(a, virus);
+                }
+                System.out.println(virus);
+                return; // 종료
             }
         }
 
-        System.out.println(testTube[X - 1][Y - 1]);
-
+        // 끝까지 전염되지 않았으면 0을 출력
+        System.out.println(0);
     }
 
     private static boolean indexCheck(int ni, int nj) {
