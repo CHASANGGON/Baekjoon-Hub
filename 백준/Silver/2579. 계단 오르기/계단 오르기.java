fbdce4cd348ea49a4cda7805cc3798f5f515c2eb
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-02-10 18:09:48 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -13,14 +13,22 @@ public class Main {
             stair[i] = Integer.parseInt(br.readLine());
         }
 
+        // 예외 처리: 계단이 한 개만 있는 경우
+        if (N == 1) {
+            System.out.println(stair[1]);
+            return;
+        }
+
         // 1. 기본 세팅
         int[] DP = new int[N + 1];
         DP[1] = stair[1];
-        if(N != 1) DP[2] = DP[1] + stair[2];
+        if (N >= 2) DP[2] = stair[1] + stair[2];  // DP[1]을 다시 참조할 필요 없음
 
         // 2. DP
-        for (int n = 3; n <= N; n++) {
-            DP[n] = stair[n] + Math.max(stair[n - 1] + DP[n - 3], DP[n - 2]);
+        if (N >= 3) {
+            for (int n = 3; n <= N; n++) {
+                DP[n] = stair[n] + Math.max(stair[n - 1] + DP[n - 3], DP[n - 2]);
+            }
         }
 
         System.out.println(DP[N]);
