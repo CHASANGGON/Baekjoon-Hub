d43dfa5a50a474122b8ebf36cbb3702da532d2d9
차상곤 <102268323+CHASANGGON@users.noreply.github.com>
2025-02-25 14:13:44 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // T 입력 받기
+        int T = Integer.parseInt(br.readLine());
+
+        // dp
+        dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        // 출력
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(dp[N]);
+        }
+    }
+}
