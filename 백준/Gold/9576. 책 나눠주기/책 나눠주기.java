68437db86320499528edf85a96a255aa52d3caa0
차상곤 <yg9618@naver.com>
2025-06-18 11:48:56 +0900

    [Gold II] Title: 책 나눠주기, Time: 204 ms, Memory: 20488 KB -BaekjoonHub

---
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // T 입력
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+
+            // N, M 입력
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            // 배열 생성
+            boolean[] used = new boolean[N + 1];
+
+            // a, b 입력
+            int[][] arr = new int[M][3];
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
+                arr[i][2] = arr[i][1] - arr[i][0] + 1; // length
+            }
+
+            // 정렬
+            Arrays.sort(arr, Comparator.comparingInt(a -> a[1]));
+
+//            for (int i = 0; i < M; i++) {
+//                System.out.println(arr[i][0] + " " + arr[i][1] + " " + arr[i][2]);
+//            }
+
+            // Greedy
+            int ans = 0;
+            for (int[] cur : arr) {
+                for (int i = cur[0]; i <= cur[1]; i++) {
+                    if (!used[i]) {
+                        used[i] = true;
+                        ans++;
+                        break;
+                    }
+                }
+            }
+
+            // 출력
+            System.out.println(ans);
+        }
+    }
+}
